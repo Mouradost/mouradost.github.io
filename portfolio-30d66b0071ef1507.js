96b42156fe2a08bb171d649e9f87fa5caaf18351
Lablack Mourad <mouradost@gmail.com>
2023-07-11 00:08:36 +0800

    bug fix

--- portfolio-30d66b0071ef1507.js
+++ portfolio-30d66b0071ef1507.js
@@ -879,7 +879,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('portfolio-4484277cb8790484_bg.wasm', import.meta.url);
+        input = new URL('portfolio-30d66b0071ef1507_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
