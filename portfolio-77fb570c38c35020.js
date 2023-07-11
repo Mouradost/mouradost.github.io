8a50b5913881e292c2700f1093f41acd01a179b9
Lablack Mourad <mouradost@gmail.com>
2023-07-11 13:32:41 +0800

    layout update

--- portfolio-77fb570c38c35020.js
+++ portfolio-77fb570c38c35020.js
@@ -879,7 +879,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('portfolio-ec2dc462b46c67ad_bg.wasm', import.meta.url);
+        input = new URL('portfolio-77fb570c38c35020_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
